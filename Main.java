e2e297d502298acb8ae3afc279d16deb7ba56d90
muhilasri <43902109+muhilasri@users.noreply.github.com>
2018-10-06 13:31:23 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+
+    public static void main(String[] args) {
+
+        int number =3;
+        if (number < 0)
+            System.out.println(number + " is a negative number.");
+
+    
+        else if ( number > 0)
+            System.out.println(number + " is a positive number.");
+
+
+        else
+            System.out.println(number + " is 0");
+    }
+}
